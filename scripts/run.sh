722c1a55e6404971c51dcabc6223fe2db06ae71b
Dishant <dishantraut@gmail.com>
2021-04-18 02:59:11 +0530

    PROD Changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -5,6 +5,6 @@ uvicorn src.app:app --reload --port 3000
 # git remote add origin git@github.com:dishantraut/fastapi_x_airtable.git
 # git push --set-upstream origin master
 # PORT=8000 heroku local
-# heroku logs -a fastapi-airtable-1
+# heroku logs -a fastapi-x-airtable
 # * How to set Config Vars / Environment Vars in Heroku from CLI
 # heroku config:set AIRTABLE_TABLE_NAME="fastapi-to-airtable" -a fastapi-airtable-1
