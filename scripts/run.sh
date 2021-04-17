2cc329ba0c53e274328fb79e00133c191412c82b
Dishant <dishantraut@gmail.com>
2021-04-18 02:21:04 +0530

    PROD Changes

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,5 +2,11 @@ uvicorn src.app:app --reload --port 3000
 # Check out python package called waitress
 # chmod +x scripts/run.sh
 # curl https://cli-assets.heroku.com/install-ubuntu.sh | sh # to install heroku
-# 
+# git remote add origin git@github.com:dishantraut/fastapi_x_airtable.git
+# git push --set-upstream origin master
+# PORT=8000 heroku local
+# heroku logs -a fastapi-airtable-1
+# * How to set Config Vars / Environment Vars in Heroku from CLI
+# heroku config:set AIRTABLE_TABLE_NAME="fastapi-to-airtable" -a fastapi-airtable-1
+
 
